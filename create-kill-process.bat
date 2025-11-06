8e48b96c76c332ef6700a1638c755041724b77b5
Alex Severins <severins.official@gmail.com>
2025-11-06 12:01:04 +0300

    ref: name and code improvements

--- create-kill-process.bat
+++ create-kill-process.bat
@@ -1,26 +1,26 @@
 @echo off
 setlocal
 
-set scriptsFolder=%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Scripts
+set "scriptsFolder=%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Scripts"
 mkdir "%scriptsFolder%" 2>nul
-set linkFolder=%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Scripts
-mkdir "%linkFolder%" 2>nul
+set "linksFolder=%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Scripts"
+mkdir "%linksFolder%" 2>nul
 
 set /p ownName=Enter a process name to create a killer shortcut: 
 
-set shortcutName=kill %ownName%.lnk
-set targetPath=%scriptsFolder%\kill %ownName%.bat
-if exist %targetPath% (
-    del %targetPath%
+set "shortcutName=kill %ownName%.lnk"
+set "targetPath=%scriptsFolder%\kill %ownName%.bat"
+if exist "%targetPath%" (
+    del /f /q "%targetPath%" >nul 2>&1
 )
 
 echo @echo off > "%targetPath%"
 echo taskkill /f /im %ownName%.exe /t >> "%targetPath%"
 echo exit >> "%targetPath%"
 
-set shortcutPath=%linkFolder%\%shortcutName%
+set "shortcutPath=%linksFolder%\%shortcutName%"
 if exist "%shortcutPath%" (
-    del "%shortcutPath%"
+    del /f /q "%shortcutPath%" >nul 2>&1
 )
 
 echo Set oWS = WScript.CreateObject("WScript.Shell") > "%temp%\createShortcut.vbs"
